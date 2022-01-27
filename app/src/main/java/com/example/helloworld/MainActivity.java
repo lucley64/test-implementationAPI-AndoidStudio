14553538216f5599cceef332dfd840929c8b1e2e
Luc Leydert <luc.leydert@gmail.com>
2022-01-27 17:30:19 +0100

    Mise en place de la recuperation du nom d'un produit à partir de son code barres

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,16 +3,11 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.InputStream;
-import java.io.StringReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,37 +24,21 @@ public class MainActivity extends AppCompatActivity {
         TextView txtHello = findViewById(R.id.txtMsg);
         EditText edtTxtName = findViewById(R.id.edtTxtName);
 
-//        String nomProduit = getProductNameFromBarCode(edtTxtName.getText().toString());
-        String nomProduit = getProductNameFromBarCode("3274080005003");
+        String nomProduit;
 
-        txtHello.setText("Nom du produit : " + nomProduit);
-
-
-    }
-    private String getProductNameFromBarCode(String barCode){
-        String ret;
-        try{
-            URL url = new URL("https://fr.openfoodfacts.org/api/v0/product/" + barCode + ".json");
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent", "ScanEco - Android - Version 0.1");
-            InputStream inputStream = connection.getInputStream(); // Crash
-            Scanner scanner = new Scanner(inputStream);
-            String json = scanner.useDelimiter("\\A").next();
-            JsonReader jsonReader = new JsonReader(new StringReader(json));
-            jsonReader.beginObject();
-            ret = "none";
-            while (jsonReader.hasNext()){
-                String name = jsonReader.nextName();
-                System.out.println(name);
-            }
-            jsonReader.close();
-            scanner.close();
-            inputStream.close();
+        try {
+            nomProduit = Product.getProductFromBarCode(edtTxtName.getText().toString()).getNom();
         }
         catch (Exception e){
-            ret = e.toString();
+            if(Objects.equals(e.getMessage(), "product not found")){
+                nomProduit = "Produit non trouvé";
+            }
+            else {
+                nomProduit = e.toString();
+            }
         }
-        return ret;
+
+        txtHello.setText("Nom du produit : " + nomProduit);
     }
 
 }
